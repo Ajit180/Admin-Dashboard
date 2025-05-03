e57fda8d43faa6012a5ff5d3d98a6a9ed10a2828
Ajit <yajit1908@gmail.com>
2025-05-03 11:13:21 +0530

    Added Homedirect navigation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
     const navigate = useNavigate();
+
+    useEffect(()=>{
+        navigate('/auth/signin')
+    },[])
   return (
-    <div>
-       This is the Home page 
-       <button onClick={navigate('/auth/siginin')}>
-          Sigin page
-       </button>
+    <div className='flex flex-wrap'>
+       This is the Home page
     </div>
   )
 }
